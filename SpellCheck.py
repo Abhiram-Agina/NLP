3ac53d41dc3819067e6045503bc2c68b879a5310
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2022-12-12 22:13:30 -0800

    Update SpellCheck.py

--- SpellCheck.py
+++ SpellCheck.py
@@ -1,1 +1,12 @@
+import streamlit as st
 from textblob import TextBlob
+
+statement = st.text_input("Enter a word, and the AI will detect which word you are typing and evaluate its spelling:") #User Input
+button = st.button("Evaluate Spelling")
+
+with st.spinner("Discovering Answers.."):
+    if button and statement:
+      if(statement == TextBlob(statement).correct()):
+        st.write("Correct spelling")
+      else:
+        st.write("Incorrect spelling. The correct spelling is: ", TextBlob(statement).correct())
