5b7e5fee513ec437450b1a0fef84d9929380422f
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2022-12-15 01:50:56 -0800

    Update SimpleSentiment.py

--- SimpleSentiment.py
+++ SimpleSentiment.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import transformers
 import pandas as pd
-from transformers import pipeline #NLP's Transformer is a new architecture that aims to solve tasks sequence-to-sequence while easily handling long-distance dependencies
+from transformers import pipeline 
+
+#NLP's Transformer is a new architecture that aims to solve tasks sequence-to-sequence while easily handling long-distance dependencies
 #A Transformer pipeline describes the flow of data from origin systems to destination systems and defines how to transform the data along the way.
 
 senti_pipeline = pipeline("sentiment-analysis")
